8c86a67605c35f657a737ffb8a42ed722ba220c7
Tomas <tosanchez@frba.utn.edu.ar>
2021-01-29 16:05:24 +0000

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/App.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/profertil/Remitos/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.profertil.Remitos.controller.App", {});
+});
