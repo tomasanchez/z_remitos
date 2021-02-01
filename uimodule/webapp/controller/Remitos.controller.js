bb7164ba2549adc380bd5b829a91069ab6dd9ea4
Tomas <tosanchez@frba.utn.edu.ar>
2021-02-01 17:52:58 +0000

    build: create remitos mvc

---
+++ uimodule/webapp/controller/Remitos.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+  "com/profertil/Remitos/controller/BaseController",
+  "sap/ui/model/json/JSONModel",
+  "../model/formatter",
+], function(Controller, JSONModel, formatter) {
+  "use strict";
+
+  return Controller.extend("com.profertil.Remitos.controller.Remitos", {
+
+      formatter: formatter
+
+    /* =========================================================== */
+		/* lifecycle methods                                           */
+    /* =========================================================== */
+
+
+  });
+
+
+});
