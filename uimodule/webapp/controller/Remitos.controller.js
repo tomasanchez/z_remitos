354a512b5351e727fa9fd12720ac45d6c1a19dac
Tomas <tosanchez@frba.utn.edu.ar>
2021-02-08 16:02:48 +0000

    feat: add cross application navigation

--- uimodule/webapp/controller/Remitos.controller.js
+++ uimodule/webapp/controller/Remitos.controller.js
@@ -19,17 +19,11 @@ sap.ui.define([
 		 * @public
 		 */
 		onInit: function () {
-			var oViewModel,
-			// eslint-disable-next-line no-unused-vars
-				iOriginalBusyDelay;
+			var oViewModel;
 
 			// eslint-disable-next-line no-unused-vars
-			oController = this;
+      oController = this;
 
-			// Put down remitos table's original value for busy indicator delay,
-			// so it can be restored later on. Busy handling on the table is
-			// taken care of by the table itself.
-	    //	iOriginalBusyDelay = oTable.getBusyIndicatorDelay();
 			// keeps the search state
 			this._aTableSearchState = [];
 
@@ -40,15 +34,6 @@ sap.ui.define([
 			});
 			this.setModel(oViewModel, "remitosView");
 
-
-			// Make sure, busy indication is showing immediately so there is no
-			// break after the busy indication for loading the view's meta data is
-			// ended (see promise 'oWhenMetadataIsLoaded' in AppController)
-		//	oTable.attachEventOnce("updateFinished", function () {
-				// Restore original busy indicator delay for remitos's table
-		//		oViewModel.setProperty("/tableBusyDelay", iOriginalBusyDelay);
-  //		});
-
 			// Add the remitos page to the flp routing history
 			this.addHistoryEntry({
 				title: this.getResourceBundle().getText("remitosViewTitle"),
@@ -58,18 +43,27 @@ sap.ui.define([
 
     },
 
+    /* =========================================================== */
+		/* event handlers                                              */
+    /* =========================================================== */
+
+    /**
+		 * Cross Application Navigation
+		 * @function
+		 * @param {sap.ui.base.Event} oEvent the event of link press
+		 * @public
+		 */
     onPress: function (oEvent) {
       if (sap.ushell && sap.ushell.Container && sap.ushell.Container.getService) {
-        var item = oEvent.getParameter("listItem").getBindingContext().getProperty("item");
+        var sRemitoID = oEvent.getSource().getText();
+        //var item = oEvent.getParameter("listItem").getBindingContext().getProperty("item");
         sap.ushell.Container.getService("CrossApplicationNavigation").toExternal({
           target: {
-            shellHash: "DocumentosRel-display?&/Documento/REM," + item
+            shellHash: "DocumentosRel-display?&/Documento/REM," + sRemitoID
           }
         });
       }
     }
-
-
 
   });
 
