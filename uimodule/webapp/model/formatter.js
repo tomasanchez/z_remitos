b5af7f1f9a97ffa1bea5e7a02a282cf0d3cc9a18
Tomas <tosanchez@frba.utn.edu.ar>
2021-02-08 14:29:09 +0000

    chore: release

--- uimodule/webapp/model/formatter.js
+++ uimodule/webapp/model/formatter.js
@@ -1,4 +1,10 @@
 sap.ui.define([], function () {
 	"use strict";
-	return {};
+	return {
+
+    dateFormat: (sDate) =>{
+      return new Date(sDate.slice(0, 4), sDate.slice(4, 6), sDate.slice(6, 8)).toLocaleDateString();
+    }
+
+  };
 });
